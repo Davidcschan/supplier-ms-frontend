9c72a8f0e5f866adf2ad5dc9dfc9895eadb4d408
davidcschan <cs.chan1999@gmail.com>
2020-08-09 15:37:46 +0800

    Updates

---
+++ precache-manifest.3708e9987c0a2efa67f5cfbf0325e638.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda418c0cc97eaebe4587392d6e1b4ff",
+    "url": "/supplier-ms-frontend/index.html"
+  },
+  {
+    "revision": "6229f2d9e47e1ab1d83d",
+    "url": "/supplier-ms-frontend/static/css/2.d74e8006.chunk.css"
+  },
+  {
+    "revision": "b1f6af28c5e5a5f2c79c",
+    "url": "/supplier-ms-frontend/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6229f2d9e47e1ab1d83d",
+    "url": "/supplier-ms-frontend/static/js/2.d3d4dee0.chunk.js"
+  },
+  {
+    "revision": "0f5abaadb162b388d3d611826ec4cf6a",
+    "url": "/supplier-ms-frontend/static/js/2.d3d4dee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9f25d6760b63c58ceb",
+    "url": "/supplier-ms-frontend/static/js/3.d5a6b417.chunk.js"
+  },
+  {
+    "revision": "32b897d528618c98989a",
+    "url": "/supplier-ms-frontend/static/js/4.4f87332f.chunk.js"
+  },
+  {
+    "revision": "6dea15728374ada202c3",
+    "url": "/supplier-ms-frontend/static/js/5.b99b4ef1.chunk.js"
+  },
+  {
+    "revision": "29d1a3b80cfb8e33baa8",
+    "url": "/supplier-ms-frontend/static/js/6.6dbc2c24.chunk.js"
+  },
+  {
+    "revision": "b47d310a7f836a2ee077",
+    "url": "/supplier-ms-frontend/static/js/7.c520052d.chunk.js"
+  },
+  {
+    "revision": "e3a20162cf647a0575ef",
+    "url": "/supplier-ms-frontend/static/js/8.d4937990.chunk.js"
+  },
+  {
+    "revision": "b1f6af28c5e5a5f2c79c",
+    "url": "/supplier-ms-frontend/static/js/main.9394f4c6.chunk.js"
+  },
+  {
+    "revision": "f37d50f9cbc201defa80",
+    "url": "/supplier-ms-frontend/static/js/runtime-main.0d7d3472.js"
+  },
+  {
+    "revision": "8b755b7f877aab8cf87b892eb6d9dbe4",
+    "url": "/supplier-ms-frontend/static/media/template.8b755b7f.csv"
+  }
+]);
---
+++ static/js/7.c520052d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsupplier-ms-admin"]=this["webpackJsonpsupplier-ms-admin"]||[]).push([[7],{1385:function(e,s,t){"use strict";t.r(s),s.default={ra:{action:{delete:"Delete",show:"Show",list:"List",save:"Save",create:"Create",edit:"Edit",cancel:"Cancel"}}}}}]);
+//# sourceMappingURL=7.c520052d.chunk.js.map
